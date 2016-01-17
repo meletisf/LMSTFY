600111e5c4b1215089b3adcbafcf4fcf3f32ec09
Meletis <fmeletis85@gmail.com>
2016-01-17 16:19:35 +0200

    Update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,5 @@
-$( document ).ready(function() {
-    
+function _get() {
+
 	var $_GET = {};
 
 	document.location.search.replace(/\??(?:([^=]+)=([^&]*)&?)/g, function() {
@@ -10,7 +10,15 @@ $( document ).ready(function() {
 		$_GET[decode(arguments[1])] = decode(arguments[2]);
 	});
 
-	if (!$_GET["keyphrase"]) {
+	return $_GET;
+
+}
+
+$( document ).ready(function() {
+    
+	var get = _get();
+
+	if (!get.keyphrase) {
 		return;
 	}
 	
@@ -18,42 +26,52 @@ $( document ).ready(function() {
 	$('#troll_well').slideDown('slow');
 	$("#pointer").show();
 	$('*').css('cursor', 'none');
-	var pos = $('input[id=search_bar_input]').offset();
-	$("#pointer").animate({
-		left: pos.left + 150,
-		top: pos.top + 20
-	}, 2500, function() {
-		
-		$('#troll_alert').html('<strong>Γράψε στο κουτάκι αυτό που θες να βρεις.</strong>');
-      	$("#search_bar_input").typed({
-        	strings: [$_GET["keyphrase"]],
-        	typeSpeed: 100,
-			showCursor: true,
-        	cursorChar: "|",
-			callback: function() {
-				pos = $('#sub_btn').offset();
-				$("#pointer").animate({
-					left: pos.left + 50,
-					top: pos.top + 15
-				}, 2500, function() {
-					$('#troll_alert').html('<strong>Μετά κάνε κλικ στο κουμπί "Αναζήτηση".</strong>')
-					setTimeout(function() {
-						$('#troll_alert').html('<strong>Δεν είναι τόσο δύσκολο, έτσι?</strong>')
+	// you need to wait few ms for the page to render in order
+	// to get the correct offset. 
+	setTimeout(function() {
+		var pos = $('input[id=search_bar_input]').offset();
+		$("#pointer").animate({
+			left: pos.left + 150,
+			top: pos.top + 20
+		}, 2500, function() {
+			
+			$('#troll_alert').html('<strong>Γράψε στο κουτάκι αυτό που θες να βρεις.</strong>');
+	      	$("#search_bar_input").typed({
+	        	strings: [get.keyphrase],
+	        	typeSpeed: 100,
+				showCursor: true,
+	        	cursorChar: "|",
+				callback: function() {
+					pos = $('#sub_btn').offset();
+					$("#pointer").animate({
+						left: pos.left + 50,
+						top: pos.top + 15
+					}, 2500, function() {
+						$('#troll_alert').html('<strong>Μετά κάνε κλικ στο κουμπί "Αναζήτηση".</strong>')
 						setTimeout(function() {
-							window.location.href = "https://www.skroutz.gr/search?keyphrase=" + $_GET["keyphrase"];
-						}, 1500);
-					}, 1500);
-				});
-			}
-      	});
-	});
+							$('#troll_alert').html('<strong>Δεν είναι τόσο δύσκολο, έτσι?</strong>')
+							setTimeout(function() {
+								window.location.href = "https://www.skroutz.gr/search?keyphrase=" + get.keyphrase;
+							}, 1200);
+						}, 1200);
+					});
+				}
+	      	});
+		});
+	}, 500);
+	
 	
 
 });
 
 function magic() {
 	
-	
+	var get = _get();
+	// if there is a keyphrase kill magic()
+	if (typeof get.keyphrase != 'undefined') {
+		return;
+	};
+
 	$('#well').hide();
 	var searchTerm = $('input[id=search_bar_input]').val();
 	
